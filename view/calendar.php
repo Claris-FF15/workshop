2a1fc790590704facbec559ee0e4354e05b6e5b6
Clarisse <clarisse.binard@icloud.com>
2025-03-31 10:36:36 +0200

    régler style !!

--- view/calendar.php
+++ view/calendar.php
@@ -6,17 +6,67 @@
     <title>Girly Plan / Calendar</title>
     <link rel="icon" href="../view/icon.png" type="image/png">
     
-    <!-- CSS -->
-    <link rel="stylesheet"href="../style/calendar.css">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Bootstrap Icons -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="../style/main.css">
+  <link rel="stylesheet"href="../style/calendar.css">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <main>
+    <section class="d-flex flex-column flex-shrink-0 bg-light Sidebar">
+      <ul class="nav flex-column mb-auto align-items-center">
+        <li>
+          <a href="../public/index.php" class="nav-link active py-4 border-bottom rounded-0">
+            <i class="bi bi-house-door"></i>
+          </a>
+        </li>
+        <li>
+          <a href="../view/archive.php" class="nav-link py-4 border-bottom rounded-0">
+            <i class="bi bi-archive"></i>
+          </a>
+        </li>
+        <li>
+          <a href="../public/index.php?action=create" class="nav-link py-4 border-bottom rounded-0">
+            <i class="bi bi-window-plus"></i>
+          </a>
+        </li>
+        <li>
+          <a href="../view/calendar.php" class="nav-link py-4 border-bottom rounded-0">
+            <i class="bi bi-calendar-week"></i>
+          </a>
+        </li>
+      </ul>
+      <div class="dropdown border-top">
+        <a href="#" class="d-flex align-items-center justify-content-center p-4 link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <img src="https://i.pinimg.com/736x/cf/17/69/cf1769f499bed0b34d0608086e865b76.jpg" alt="user" width="30" height="30" class="rounded-circle">
+        </a>
+        <ul class="dropdown-menu text-small shadow">
+          <li><a class="dropdown-item" href="../public/index.php?action=profile" style="color:rosybrown;font-family: 'Playfair Display', serif;
+          font-style: italic;">Profil</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="../public/index.php?action=logout" style="color:rosybrown;font-family: 'Playfair Display', serif;
+          font-style: italic;">Déconnexion</a></li>
+        </ul>
+      </div>
+    </section>
+<section style="margin-top:100px;">
         <div class="header">
             <button onclick="prevMonth()">◀ Mois précédent</button>
-            <h2 id="monthYear"></h2>
+            <h3 id="monthYear"></h3>
             <button onclick="nextMonth()">Mois suivant ▶</button>
         </div>
-        <div class="calendar" id="calendar"></div>
+        <div class="calendar mb-3" id="calendar"></div>
         
         <script>
             let currentDate = new Date();
@@ -101,6 +151,9 @@
             renderCalendar();
         </script>
     </main>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
 
